95b21c0cadd0e648df7245d4c6d0375286c45ac0
morioka <r.morika@icloud.com>
2021-11-26 02:01:04 +0900

    GOLFERS24のおすすめをカスタムフィールドで作成

--- features.php
+++ features.php
@@ -6,110 +6,39 @@
   <!-- デスクトップ用交互に紹介するGOLFERS24の特徴 -->
   <div class="second-section-features">
 
-    <!-- 特徴１ -->
-    <div class="feature">
-      <div class="feature-text-wrapper">
-        <div class="feature-text-box">
-          <div class="feature-title">
-            <p>あなただけの</p>
-            <p>完全個室型会員ゴルフラウンジ</p>
-          </div>
-          <div class="feature-sentence">
-            <p>
-              室内は1打席のみの完全プライベート空間。空調の整ったラグジュアリーな空間で、
-              黙々と練習に打ち込むことはもちろん、好きな音楽を流しながら練習を楽しんだり、
-              家族や恋人、親子で楽しみながら練習することも可能です。
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="feature-image"><img src="<?php echo get_template_directory_uri(); ?>/images/feature1.png" alt=""></div>
-    </div>
+    <?php
+    $args = array(
+      'post_type' => 'feature',
+      // 全件取得、数を指定すればその数だけ取得する。
+      'posts_per_page' => -1,
+      'order' => 'ASC'
+    );
+    $st_query = new WP_Query($args);
+    ?>
 
-    <!-- 特徴２ -->
-    <div class="feature">
-      <div class="feature-text-wrapper">
-        <div class="feature-text-box">
-          <div class="feature-title">
-            <p>24時間いつでも</p>
-            <p>手ぶらで通い放題</p>
-          </div>
-          <div class="feature-sentence">
-            <p>
-              24時間無人での運営を実現し、いつでも好きな時に練習可能な環境を整えています。
-              クラブも最新のレンタルクラブを取り揃えているため、
-              お仕事帰りでもスーツのまま手ぶらで気軽にお越しいただけます。
-            </p>
-            <p>好きな時に気軽に練習することができるため、ゴルフをより日常のものとして楽しむことができます。</p>
-          </div>
-        </div>
-      </div>
-      <div class="feature-image"><img src="<?php echo get_template_directory_uri(); ?>/images/feature2.png" alt=""></div>
-    </div>
+    <?php if ($st_query->have_posts()) : ?>
+      <?php while ($st_query->have_posts()) : $st_query->the_post(); ?>
 
-    <!-- 特徴３ -->
-    <div class="feature">
-      <div class="feature-text-wrapper">
-        <div class="feature-text-box">
-          <div class="feature-title">
-            <p>世界シェアNo.1の</p>
-            <p>ゴルフシミュレーターを採用</p>
-          </div>
-          <div class="feature-sentence">
-            <p>
-              米LPGA公認のGOLFZON『GDR』を採用。スイングデータを正確に数値化し確認することができるので、
-              効率良くスコアアップを目指すことができます。
-            </p>
-            <p>
-              GOLFZONアプリに登録するとスイング動画や練習結果を自動で保存してくれるため、
-              いつでもどこでも自身のスイングやデータを確認し練習に活かすことが可能です。
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="feature-image"><img src="<?php echo get_template_directory_uri(); ?>/images/feature3.png" alt=""></div>
-    </div>
 
-    <!-- 特徴４ -->
-    <div class="feature">
-      <div class="feature-text-wrapper">
-        <div class="feature-text-box">
-          <div class="feature-title">
-            <p>スムーズな即時予約と万全のセキュリティ</p>
+        <div class="feature">
+          <div class="feature-text-wrapper">
+            <div class="feature-text-box">
+              <div class="feature-title">
+                <p><?php echo SCF::get('feature-title') ?></p>
+              </div>
+              <div class="feature-sentence">
+                <p><?php echo SCF::get('feature-content') ?></p>
+              </div>
+            </div>
           </div>
-          <div class="feature-sentence">
-            <p>
-              予約時には面倒なスタッフとのやりとりも一切無し。いつでも専用の予約システムから簡単に予約を取ることができます。
-              入退場は予約時にスマホアプリ上で発行される電子錠を扉にかざすだけでOK。
-              予約から入場、退場までを全てご自身でストレスなく行っていただけます。
-              また、電子鍵は自身が予約した時間でのみ有効となるため、練習中に別の会員様が入ってくるような心配も無し。
-              セキュリティ面も万全で安心して練習に集中いただくことができます。
-            </p>
-          </div>
+          <div class="feature-image"><?php $image = SCF::get('image');
+                                      echo wp_get_attachment_image($image, 'large'); ?></div>
         </div>
-      </div>
-      <div class="feature-image"><img src="<?php echo get_template_directory_uri(); ?>/images/feature4.png" alt=""></div>
-    </div>
 
-    <!-- 特徴５ -->
-    <div class="feature">
-      <div class="feature-text-wrapper">
-        <div class="feature-text-box">
-          <div class="feature-title">
-            <p>万全の感染防止対策</p>
-          </div>
-          <div class="feature-sentence">
-            <p>
-              安心してご利用いただけるよう、手指のアルコール消毒はもちろん、
-              1分間で99.99%以上の新型コロナウイルスを不活性化させる効果を持つクリーン・リフレを採用。
-              空気中に拡散することで空間そのものの感染防止対策を行っております。
-              原料は水と食塩のみのため、当然人体にも無害な成分です。
-            </p>
-          </div>
-        </div>
-      </div>
-      <div class="feature-image"><img src="<?php echo get_template_directory_uri(); ?>/images/feature5.png" alt=""></div>
-    </div>
+      <?php endwhile; ?>
+    <?php endif; ?>
+
+
 
   </div>
 
@@ -118,115 +47,47 @@
     <div class="swiper">
       <div class="swiper-wrapper">
 
-        <!-- 特徴１ -->
-        <div class="feature swiper-slide">
-          <div class="feature-text-wrapper">
-            <div class="feature-text-box">
-              <div class="feature-title">
-                <p>あなただけの</p>
-                <p>完全個室型会員ゴルフラウンジ</p>
-              </div>
-              <div class="feature-sentence">
-                <p>
-                  室内は1打席のみの完全プライベート空間。空調の整ったラグジュアリーな空間で、
-                  黙々と練習に打ち込むことはもちろん、好きな音楽を流しながら練習を楽しんだり、
-                  家族や恋人、親子で楽しみながら練習することも可能です。
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="feature-image"><img src="<?php echo get_template_directory_uri(); ?>/images/feature1.png" alt=""></div>
-        </div>
+        <?php
+        $args = array(
+          'post_type' => 'feature',
+          // 全件取得、数を指定すればその数だけ取得する。
+          'posts_per_page' => -1,
+          'order' => 'ASC'
+        );
+        $st_query = new WP_Query($args);
+        ?>
 
-        <!-- 特徴２ -->
-        <div class="feature swiper-slide">
-          <div class="feature-text-wrapper">
-            <div class="feature-text-box">
-              <div class="feature-title">
-                <p>24時間いつでも</p>
-                <p>手ぶらで通い放題</p>
-              </div>
-              <div class="feature-sentence">
-                <p>
-                  24時間無人での運営を実現し、いつでも好きな時に練習可能な環境を整えています。
-                  クラブも最新のレンタルクラブを取り揃えているため、
-                  お仕事帰りでもスーツのまま手ぶらで気軽にお越しいただけます。
-                </p>
-                <p>好きな時に気軽に練習することができるため、ゴルフをより日常のものとして楽しむことができます。</p>
-              </div>
-            </div>
-          </div>
-          <div class="feature-image"><img src="<?php echo get_template_directory_uri(); ?>/images/feature2.png" alt=""></div>
-        </div>
+        <?php if ($st_query->have_posts()) : ?>
+          <?php while ($st_query->have_posts()) : $st_query->the_post(); ?>
 
-        <!-- 特徴３ -->
-        <div class="feature swiper-slide">
-          <div class="feature-text-wrapper">
-            <div class="feature-text-box">
-              <div class="feature-title">
-                <p>世界シェアNo.1の</p>
-                <p>ゴルフシミュレーターを採用</p>
+            <!-- 特徴１ -->
+            <div class="feature swiper-slide">
+              <div class="feature-text-wrapper">
+                <div class="feature-text-box">
+                  <div class="feature-title">
+                    <p><?php echo SCF::get('feature-title') ?></p>
+                  </div>
+                  <div class="feature-sentence">
+                    <p>
+                    <p><?php echo SCF::get('feature-content') ?></p>
+                    </p>
+                  </div>
+                </div>
               </div>
-              <div class="feature-sentence">
-                <p>
-                  米LPGA公認のGOLFZON『GDR』を採用。スイングデータを正確に数値化し確認することができるので、
-                  効率良くスコアアップを目指すことができます。
-                </p>
-                <p>
-                  GOLFZONアプリに登録するとスイング動画や練習結果を自動で保存してくれるため、
-                  いつでもどこでも自身のスイングやデータを確認し練習に活かすことが可能です。
-                </p>
-              </div>
+              <div class="feature-image"><?php $image = SCF::get('image');
+                                          echo wp_get_attachment_image($image, 'large'); ?></div>
             </div>
-          </div>
-          <div class="feature-image"><img src="<?php echo get_template_directory_uri(); ?>/images/feature3.png" alt=""></div>
-        </div>
 
-        <!-- 特徴４ -->
-        <div class="feature swiper-slide">
-          <div class="feature-text-wrapper">
-            <div class="feature-text-box">
-              <div class="feature-title">
-                <p>スムーズな即時予約と万全のセキュリティ</p>
-              </div>
-              <div class="feature-sentence">
-                <p>
-                  予約時には面倒なスタッフとのやりとりも一切無し。いつでも専用の予約システムから簡単に予約を取ることができます。
-                  入退場は予約時にスマホアプリ上で発行される電子錠を扉にかざすだけでOK。
-                  予約から入場、退場までを全てご自身でストレスなく行っていただけます。
-                  また、電子鍵は自身が予約した時間でのみ有効となるため、練習中に別の会員様が入ってくるような心配も無し。
-                  セキュリティ面も万全で安心して練習に集中いただくことができます。
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="feature-image"><img src="<?php echo get_template_directory_uri(); ?>/images/feature4.png" alt=""></div>
-        </div>
 
-        <!-- 特徴５ -->
-        <div class="feature swiper-slide">
-          <div class="feature-text-wrapper">
-            <div class="feature-text-box">
-              <div class="feature-title">
-                <p>万全の感染防止対策</p>
-              </div>
-              <div class="feature-sentence">
-                <p>
-                  安心してご利用いただけるよう、手指のアルコール消毒はもちろん、
-                  1分間で99.99%以上の新型コロナウイルスを不活性化させる効果を持つクリーン・リフレを採用。
-                  空気中に拡散することで空間そのものの感染防止対策を行っております。
-                  原料は水と食塩のみのため、当然人体にも無害な成分です。
-                </p>
-              </div>
-            </div>
-          </div>
-          <div class="feature-image"><img src="<?php echo get_template_directory_uri(); ?>/images/feature5.png" alt=""></div>
-        </div>
+
+            <div class="swiper-pagination"></div>
+
+
+
+          <?php endwhile; ?>
+        <?php endif; ?>
       </div>
 
-      <div class="swiper-pagination"></div>
 
     </div>
-
   </div>
-</div>
