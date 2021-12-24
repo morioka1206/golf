3c3ca44771801523605b747e8e1d529a742362ee
morioka1206 <40672270+morioka1206@users.noreply.github.com>
2021-12-24 18:56:08 +0900

    モバイルのthirdセクションを実装

--- third-section.php
+++ third-section.php
@@ -1,15 +1,13 @@
 <div class="third-section">
   <div class="third-section-wrapper">
     <div class="third-section-how-to-use-text">
-      <!-- <div class="how-to-use-title">
-        ご利用の流れ・体験予約
-      </div> -->
       <div class="how-to-use-sentence">
         <p>GOLFERS24ご利用の流れを紹介しております。本利用前にトライアル利用も可能。</p>
       </div>
       <div class="sm-how-to-use-sentence">
-        <p>GOLFERS24ご利用の流れを紹介しております。
-          本利用前にトライアル利用も可能。</p>
+        <p>GOLFERS24ご利用の流れを</p>
+        <p>紹介しております。</p>
+        <p>本利用前にトライアル利用も可能。</p>
       </div>
     </div>
 
@@ -18,12 +16,11 @@
       <div class="third-section-how-to-use-arrow"></div>
     </a>
 
-  </div>
-  <div class="third-section-how-to-use-image"><a href="<?php echo esc_url(home_url('/how-to-use')); ?>"><img src="<?php echo get_theme_file_uri(); ?>/images/howtouse-arrow.svg" alt=""></a></div>
-  <!-- <a href="<?php echo esc_url(home_url('/how-to-use')); ?>">
-    <div class="third-section-arrow-wrapper">
-      <p>VIEW MORE</p>
+    <a href="<?php echo esc_url(home_url('/how-to-use')); ?>" class="sm-third-section-how-to-use-button">
+      <p>ご利用の流れ・</p>
+      <p>体験予約はこちら</p>
       <div class="third-section-how-to-use-arrow"></div>
-    </div>
-  </a> -->
+    </a>
+
+  </div>
 </div>
